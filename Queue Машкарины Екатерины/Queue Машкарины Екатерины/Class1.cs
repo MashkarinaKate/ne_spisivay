68c1d44805132a1d2b398568ed250d898f2c638d
MashkarinaKate <mashkarinakate@gmail.com>
2020-11-25 20:43:32 +0300

    d

---
+++ Queue Машкарины Екатерины/Queue Машкарины Екатерины/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Машкарины_Екатерины
+{
+    public class Queue<T>
+    {
+        public T[] array;
+        int head;
+        int tail;
+        public int size;
+        int capacity;
+        public Queue()
+        {
+            size = 0;
+            head = 0;
+            tail = -1;
+            array = new T[1];
+            capacity = 1;
+        }
+        public bool isEmpty()
+        {
+            return (size == 0);
+        }
+        public void Enqueue(T item)
+        {
+            if (item == null)
+            {
+                throw new Exception("item is null");
+            }
+            tail++;
+            size++;
+            capacity++;
+            T[] array2 = new T[capacity];
+            Array.Copy(array, array2, tail);
+            array2[tail] = item;
+            array = new T[size];
+            Array.Copy(array2, array, size);
+        }
+        public T Dequeue()
+        {
+            if (isEmpty())
+            {
+                throw new Exception();
+            }
+            head++;
+            size--;
+            capacity--;
+            return array[head -1];
+        }
+        public T Peek()
+        {
+            if (isEmpty())
+            {
+                throw new Exception();
+            }
+            return array[head];
+        }
+        public void Clear()
+        {
+            array = new T[1];
+            head = 0;
+            size = 0;
+        }
+        public int Count
+        {
+            get { return size; }
+        }
+    }
+}
+
---
+++ Queue Машкарины Екатерины/Queue Машкарины Екатерины/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Машкарины_Екатерины
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            bool s = true;
+            int x;
+            Console.Write(@"
+Введите:
+1 - Enqueue
+2 - Deuque
+3 - Peek
+4 - Clear
+5 - Count
+6 - End" + "\n");
+            while (s)
+            {
+                x = int.Parse(Console.ReadLine());
+                switch (x)
+                {
+                    case (1): Console.WriteLine("Input: ");
+                              x = int.Parse(Console.ReadLine());
+                              q.Enqueue(x); break;
+                    case (2):
+                        try
+                        {
+                            Console.WriteLine("Head: {0}", q.Dequeue());
+                        }
+                        catch { Console.Write("Queue is empty"); }
+                        break;
+                    case (3):
+                        try
+                        {
+                            Console.WriteLine("Head of this queue: {0}", q.Peek());
+                        }
+                        catch { Console.Write("Queue is empty"); }
+                        break;
+                    case (4): q.Clear(); break;
+                    case (5): Console.WriteLine("Count: {0}", q.Count); break;
+                    case (6): s = false; break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
